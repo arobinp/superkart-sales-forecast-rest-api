11057ea8d7fea40ad78c64e1550e9191a504e991
arobinp <arobinp@gmail.com>
2026-09-23 03:56:06 +0000

    Fix model loading path

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify  # For creating the Flask API
 sales_forecast_predictor_api = Flask("Sales Forcast Predictor")
 
 # Load the trained machine learning model
-model = joblib.load("/content/drive/MyDrive/GreatLearning/ModelDeployment/backend_files/sales_forecast_prediction_model_v2_0.joblib")
+model = joblib.load("sales_forecast_prediction_model_v2_0.joblib")
 
 # Define a route for the home page (GET request)
 @sales_forecast_predictor_api.get('/')
